43666b90a95cd02b2a942bc789cd009062c79ff0
V_Fugere <vincent.fugere@mail.mcgill.ca>
2020-04-17 17:24:07 -0400

    fig 2

--- foodweb_paper.R
+++ foodweb_paper.R
@@ -37,15 +37,20 @@ treat <- read.csv('~/Google Drive/Recherche/LEAP Postdoc/2016/raw data/LEAP2016t
   select(-nut.ug.P,-nut.rel,-array,-nb)
 treat$nut.fac <- factor(treat$nut, levels = c('low','high'))
 treat$nut.num <- as.numeric(treat$nut.fac)
+treat$pond.id <- 1:48
 
-colfunc <- colorRampPalette(c("gray80", "#99000D"))
-gly.cols <- colfunc(8)
-colfunc <- colorRampPalette(c("gray80", "#084594"))
-imi.cols <- colfunc(8)
-colfunc <- colorRampPalette(c("gray80", "#005A32"))
-both.cols <- colfunc(8)
+library(wesanderson)
+#three.cols <- c('#058A51','#130CA6','#EB0C00')
+three.cols <- wes_palette('Zissou1',5)[c(3,1,5)]
+glycolfunc <- colorRampPalette(c("gray80", three.cols[1]))
+gly.cols <- glycolfunc(8)
+imicolfunc <- colorRampPalette(c("gray80", three.cols[2]))
+imi.cols <- imicolfunc(8)
+bothcolfunc <- colorRampPalette(c("gray80", three.cols[3]))
+both.cols <- bothcolfunc(8)
+
+allcols <- c(gly.cols,gly.cols,imi.cols,imi.cols,both.cols,both.cols)
 pchs <- c(1,0)
-rm(colfunc)
 
 #### nutrient and pesticide data ####
 
@@ -129,12 +134,28 @@ data <- inner_join(FP,YSI, by = c('date','site')) %>%
 
 #### Figure: did treatments work? ####
 
+nut <- left_join(nut, treat)
+
+imi.lake <- filter(imi, site == 'LAKE')
+gly.lake <- filter(gly, site == 'LAKE')
+
+gly <- gly %>% left_join(treat, by = 'site') %>% filter(site %!in% c('E1','H1','LAKE'), date < 45)
+gly$gly.measured.ppm <- gly$gly.measured.ppb/1000
+gly$log.gly.measured <- log10p(gly$gly.measured.ppb)
+gly$log.gly.target <- log10p(gly$gly.target.ppb)
+
+imi <- imi %>% left_join(treat, by = 'site') %>% filter(site %!in% c('C5','D1','LAKE'), date < 45)
+imi$log.imi.measured <- log10p(imi$imi.measured.ppb)
+imi$log.imi.target <- log10p(imi$imi.target.ppb)
+
+######
+
 ## nutrients
 
-pdf('~/Desktop/figure2.pdf',width=10,height = 8,pointsize = 12)
-par(mfrow=c(3,2),cex=1,mar=c(4,4,1,1))
+pdf('~/Desktop/figure2.pdf',width=11.5,height = 8.5,pointsize = 12)
+layout(rbind(c(1,1,2,2),c(3,3,4,5),c(6,6,7,8)))
+par(cex=1,mar=c(4,4,1,1))
 
-nut <- left_join(nut, treat)
 tmp <- filter(nut, gly == 1, imi == 1, site != 'LAKE')
 lake <- filter(nut, site == 'LAKE')
 
@@ -155,7 +176,6 @@ legend(x=7,y=max(tmp$TN),bty='n',legend=c('low nutrient','high nutrient','source
 text(x=pulse.dates[1],y=max(tmp$TN)*1.05,label=expression(italic(dose~1)),pos=4)
 text(x=pulse.dates[2],y=max(tmp$TN)*1.05,label=expression(italic(dose~2)),pos=4)
 
-
 #TP
 plotfunctions::emptyPlot(xlim=c(5,43),ylim=range(tmp$TP)*c(0.9,1.1),yaxt='n',xaxt='n',ann=F, bty='l', log = 'y')
 axis(2,cex.axis=1,lwd=0,lwd.ticks=1)
@@ -169,7 +189,7 @@ for(i in 1:6){
   points(x=sub.sub$date,y=sub.sub$TP,type='o',lwd=1,pch=pchs[sub.sub$nut.num[1]],col=gly.cols[1])
 }
 points(x=lake$date,y=lake$TP,type='o',lwd=1,pch=16,col=1)
-legend(x=7,y=max(tmp$TP),bty='n',legend=c('low nutrient','high nutrient','source lake'),pch=c(1,0,16),lty=1,col=c('gray80','gray80','black'))
+#legend(x=7,y=max(tmp$TP),bty='n',legend=c('low nutrient','high nutrient','source lake'),pch=c(1,0,16),lty=1,col=c('gray80','gray80','black'))
 text(x=pulse.dates[1],y=max(tmp$TP)*1.05,label=expression(italic(dose~1)),pos=4)
 text(x=pulse.dates[2],y=max(tmp$TP)*1.05,label=expression(italic(dose~2)),pos=4)
 
@@ -195,24 +215,6 @@ text(x=pulse.dates[2],y=max(tmp$TP)*1.05,label=expression(italic(dose~2)),pos=4)
 
 ## pesticides
 
-gly <- read_xlsx('~/Google Drive/Recherche/LEAP Postdoc/2016/raw data/Contaminants/gly_clean.xlsx') %>% select(-time.point, -gly.expected.ppb, -gly.max.ppb)
-gly$date <- gly$date %>% as.Date(format = '%d.%m.%Y') %>% format('%j') %>% as.numeric
-gly$date <- gly$date - 229
-
-imi <- read_xlsx('~/Google Drive/Recherche/LEAP Postdoc/2016/raw data/Contaminants/imi_clean.xlsx') %>% select(-time.point, -notes)
-imi$date <- imi$date %>% as.Date(format = '%d.%m.%Y') %>% format('%j') %>% as.numeric
-imi$date <- imi$date - 229
-
-# filter(imi, site == 'LAKE')
-# filter(gly, site == 'LAKE')
-
-gly <- gly %>% left_join(treat, by = 'site') %>% filter(site %!in% c('E1','H1','LAKE'), date < 45)
-gly$gly.measured.ppm <- gly$gly.measured.ppb/1000
-gly$log.gly.measured <- log10p(gly$gly.measured.ppb)
-
-imi <- imi %>% left_join(treat, by = 'site') %>% filter(site %!in% c('C5','D1','LAKE'), date < 45)
-imi$log.imi.measured <- log10p(imi$imi.measured.ppb)
-
 ts.ponds.gly <- c('C1','C4','C8','D1','D4','D8','J4','J8','K4','K8') #ponds with 4-5 gly measurements over time 
 ts.ponds.imi <- c('E1','E4','E8','H1','H4','H8','J4','J8','K4','K8') #ponds with 4-5 imi measurements over time
 
@@ -223,7 +225,7 @@ imip1 <- filter(imi, date == 6)
 imip2 <- filter(imi, date == 34)
 imits <- filter(imi, site %in% ts.ponds.imi)
 
-plotfunctions::emptyPlot(xlim=c(5,43),ylim=range(glyts$log.gly.measured)*c(0.9,1.1),yaxt='n',xaxt='n',ann=F, bty='l')
+plotfunctions::emptyPlot(xlim=c(5,43),ylim=c(-0.5,max(glyts$log.gly.measured)*1.1),yaxt='n',xaxt='n',ann=F, bty='l')
 title(ylab=log[10](1+glyphosate)~(mu*g~L^-1),line=2.5)
 axis(2,cex.axis=1,lwd=0,lwd.ticks=1)
 axis(1,cex.axis=1,lwd=0,lwd.ticks=1)
@@ -232,22 +234,78 @@ abline(v=pulse.dates[1:2],lty=3)
 for(i in 1:10){
   pond <- unique(glyts$site)[i]
   sub.sub <- filter(glyts, site == pond)
-  points(x=sub.sub$date,y=sub.sub$TP,type='o',lwd=1,pch=pchs[sub.sub$nut.num[1]],col=gly.cols[1])
+  points(x=sub.sub$date,y=sub.sub$log.gly.measured,type='o',lwd=1,pch=pchs[sub.sub$nut.num[1]],col=allcols[sub.sub$pond.id])
 }
-points(x=lake$date,y=lake$TP,type='o',lwd=1,pch=16,col=1)
-legend(x=7,y=max(tmp$TP),bty='n',legend=c('low nutrient','high nutrient','source lake'),pch=c(1,0,16),lty=1,col=c('gray80','gray80','black'))
-text(x=pulse.dates[1],y=max(tmp$TP)*1.05,label=expression(italic(dose~1)),pos=4)
-text(x=pulse.dates[2],y=max(tmp$TP)*1.05,label=expression(italic(dose~2)),pos=4)
+points(x=gly.lake$date,y=log10p(gly.lake$gly.measured.ppb),pch=16,col=1)
+text(x=pulse.dates[1],y=max(glyts$log.gly.measured)*1.05,label=expression(italic(dose~1)),pos=4)
+text(x=pulse.dates[2],y=max(glyts$log.gly.measured)*1.05,label=expression(italic(dose~2)),pos=4)
+xseqs <- seq(37,43,length.out = 100)
+yrg <- c(-0.5,max(glyts$log.gly.measured)*1.1)
+ypts <- seq(from=yrg[1],to=yrg[2],length.out = 10)
+points(rep(ypts[3],100)~xseqs, col=bothcolfunc(100), pch=16)
+points(rep(ypts[6],100)~xseqs, col=glycolfunc(100), pch=16)
+text(x=c(40,40),y=c(ypts[3],ypts[6]),labels = make.italic(c('both pesticides','glyphosate')), pos=3,cex=0.9)
 
-for (i in 1:34){
-  tmp <- subset(gly, site == levels(gly$site)[i])
-  if(nrow(tmp) == 5){
-    points(log.gly.m~date.idx,tmp,type='l',col=alpha(cols[tmp$col],0.8),lwd=0.5,lty=tmp$lty)
-  }
-  points(log.gly.m~date.idx,tmp,type='p',pch=tmp$pch[1],col=alpha(cols[tmp$col],0.8),cex=1)
+plotfunctions::emptyPlot(xlim=range(glyp1$log.gly.target),ylim=range(glyp1$log.gly.measured),yaxt='n',xaxt='n',ann=F, bty='o')
+title(ylab=measured~(log[10]~1+ppb),line=2.5)
+title(xlab=target~(log[10]~1+ppb), cex.lab=1,line=2.8)
+axis(1,cex.axis=1,lwd=0,lwd.ticks=1)
+axis(2,cex.axis=1,lwd=0,lwd.ticks=1)
+abline(a=0,b=1,lty=2)
+points(log.gly.measured~log.gly.target,glyp1,pch=pchs[glyp1$nut.num],col=allcols[glyp1$pond.id])
+legend('topleft',bty='n',legend='dose 1',cex=0.95)
+
+plotfunctions::emptyPlot(xlim=range(glyp2$log.gly.target),ylim=range(glyp2$log.gly.measured),yaxt='n',xaxt='n',ann=F, bty='o')
+title(ylab=measured~(log[10]~1+ppb),line=2.5)
+title(xlab=target~(log[10]~1+ppb), cex.lab=1,line=2.8)
+axis(1,cex.axis=1,lwd=0,lwd.ticks=1)
+axis(2,cex.axis=1,lwd=0,lwd.ticks=1)
+abline(a=0,b=1,lty=2)
+points(log.gly.measured~log.gly.target,glyp2,pch=pchs[glyp2$nut.num],col=allcols[glyp2$pond.id])
+legend('topleft',bty='n',legend='dose 2',cex=0.95)
+
+#imi
+
+plotfunctions::emptyPlot(xlim=c(5,43),ylim=c(-0.1,max(imits$log.imi.measured)*1.1),yaxt='n',xaxt='n',ann=F, bty='l')
+title(ylab=log[10](1+imidacloprid)~(mu*g~L^-1),line=2.5)
+axis(2,cex.axis=1,lwd=0,lwd.ticks=1)
+axis(1,cex.axis=1,lwd=0,lwd.ticks=1)
+title(xlab="day", cex.lab=1,line=2.8)
+abline(v=pulse.dates[1:2],lty=3)
+for(i in 1:10){
+  pond <- unique(imits$site)[i]
+  sub.sub <- filter(imits, site == pond)
+  points(x=sub.sub$date,y=sub.sub$log.imi.measured,type='o',lwd=1,pch=pchs[sub.sub$nut.num[1]],col=allcols[sub.sub$pond.id])
 }
+points(x=imi.lake$date,y=log10p(imi.lake$imi.measured.ppb),pch=16,col=1)
+text(x=pulse.dates[1],y=max(imits$log.imi.measured)*1.05,label=expression(italic(dose~1)),pos=4)
+text(x=pulse.dates[2],y=max(imits$log.imi.measured)*1.05,label=expression(italic(dose~2)),pos=4)
+xseqs <- seq(37,43,length.out = 100)
+yrg <- c(-0.1,max(imits$log.imi.measured)*1.1)
+ypts <- seq(from=yrg[1],to=yrg[2],length.out = 10)
+points(rep(ypts[3],100)~xseqs, col=bothcolfunc(100), pch=16)
+points(rep(ypts[6],100)~xseqs, col=imicolfunc(100), pch=16)
+text(x=c(40,40),y=c(ypts[3],ypts[6]),labels = make.italic(c('both pesticides','imidacloprid')), pos=3,cex=0.9)
 
+plotfunctions::emptyPlot(xlim=range(imip1$log.imi.target),ylim=range(imip1$log.imi.measured),yaxt='n',xaxt='n',ann=F, bty='o')
+title(ylab=measured~(log[10]~1+ppb),line=2.5)
+title(xlab=target~(log[10]~1+ppb), cex.lab=1,line=2.8)
+axis(1,cex.axis=1,lwd=0,lwd.ticks=1)
+axis(2,cex.axis=1,lwd=0,lwd.ticks=1)
+abline(a=0,b=1,lty=2)
+points(log.imi.measured~log.imi.target,imip1,pch=pchs[imip1$nut.num],col=allcols[imip1$pond.id])
+legend('topleft',bty='n',legend='dose 1',cex=0.95)
 
+plotfunctions::emptyPlot(xlim=range(imip2$log.imi.target),ylim=range(imip2$log.imi.measured),yaxt='n',xaxt='n',ann=F, bty='o')
+title(ylab=measured~(log[10]~1+ppb),line=2.5)
+title(xlab=target~(log[10]~1+ppb), cex.lab=1,line=2.8)
+axis(1,cex.axis=1,lwd=0,lwd.ticks=1)
+axis(2,cex.axis=1,lwd=0,lwd.ticks=1)
+abline(a=0,b=1,lty=2)
+points(log.imi.measured~log.imi.target,imip2,pch=pchs[imip2$nut.num],col=allcols[imip2$pond.id])
+legend('topleft',bty='n',legend='dose 2',cex=0.95)
+
+dev.off()
 
 #### data exploration: correlations in the dataset ####
 
