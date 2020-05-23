29ba692f5824ec420b50681b21e965e88ba43941
V_Fugere <vincent.fugere@mail.mcgill.ca>
2020-05-23 16:13:08 -0400

    finalized models

--- SEM.R
+++ SEM.R
@@ -40,32 +40,82 @@ merged.data$sc.imi.std2 <- arm::rescale(merged.data$sc.imi)
 
 ##### SEM for NEP, time point 5 ####
 
-sub <- filter(merged.data, date == 15)
-sub$phyto <- log10(sub$total)
-sub$zoo <- log10p(sub$total_zoo)
-sub$bacterio <- log10(sub$BA)
-sub$delta.O2 <- log10p(sub$NEP)
+merged.data$phyto <- log10(merged.data$total)
+merged.data$zoo <- log10p(merged.data$total_zoo)
+merged.data$bacterio <- log10(merged.data$BA)
+merged.data$delta.O2 <- log10p(merged.data$NEP)
+merged.data$zp <- log10(merged.data$RUE)
 
-sem1 <- psem(
-  lm(zoo ~ sc.gly, data = sub),
-  lm(phyto ~ sc.gly + zoo + nut.num, data = sub),
-  lm(delta.O2 ~ sc.gly + phyto + nut.num, data = sub),
-  data = sub
-)
+results <- data.frame()
 
-summary(sem1)
+for(date.x in c(7,15,35,43)){
+  
+  sub <- filter(merged.data, date == date.x)
+  
+  sem <- psem(
+    lm(zoo ~ sc.gly, data = sub),
+    lm(phyto ~ sc.gly + nut.num + zoo, data = sub),
+    lm(delta.O2 ~ sc.gly + phyto + nut.num, data = sub),
+    data = sub
+  ) 
+  #adding bacterio to NEP never produces a significant path (although bacterio responds to treatments)
+  #reversing the path from phyto to zoo does not change anything for other paths, and it is never a significant path
+  
+  cat(paste('#\n#\n================================ DAY',date.x,'================================\n#\n#'))
+  print(summary(sem,.progressBar = F))
+  
+  subres <- cbind(date.x,coefs(sem))
+  results <- rbind(results,subres)
+  
+}
 
-sem1 <- psem(
-  lm(zoo ~ sc.imi.std2 + sc.gly.std2, data = sub),
-  lm(phyto ~ sc.gly.std2 + zoo, data = sub),
-  lm(bacterio ~ sc.gly.std2, data = sub),
-  lm(delta.O2 ~ sc.gly.std2 + phyto + bacterio + zoo, data = sub),
-  data = sub
-)
+# for plotting
 
-sem1  
-basisSet(sem1)  
-dSep(sem1)
-fisherC(sem1)
+results$arrow.size <- 0.5
+results$arrow.size[results$P.Value < 0.05]  <- scales::rescale(results$Std.Estimate[results$P.Value < 0.05], c(1,3.5))
+results
 
-summary(sem1)
+##### exploring other variables ####
+
+#rue
+
+for(date.x in c(7,15,30,35,43)){
+  sub <- filter(merged.data, date == date.x)
+  sub <- sub[is.finite(sub$zp),]
+  sem <- psem(
+    lm(zoo ~ sc.gly*sc.imi + phyto, data = sub),
+    lm(phyto ~ sc.gly + nut.num, data = sub),
+    lm(zp ~ phyto + zoo, data = sub),
+    data = sub
+  )
+  cat(paste('#\n#\n================================ DAY',date.x,'================================\n#\n#'))
+  print(summary(sem,.progressBar = F))
+}
+
+# zoo bottom up (phyto-zoo path only significant at one TP, )
+
+for(date.x in c(7,15,30,35,43)){
+  sub <- filter(merged.data, date == date.x)
+  sub <- sub[is.finite(sub$zp),]
+  sem <- psem(
+    lm(zoo ~ sc.gly*sc.imi + phyto, data = sub),
+    lm(phyto ~ sc.gly + nut.num, data = sub),
+    data = sub
+  ) 
+  cat(paste('#\n#\n================================ DAY',date.x,'================================\n#\n#'))
+  print(summary(sem,.progressBar = F))
+}
+
+# zoo top-down (worsens AIC)
+
+for(date.x in c(7,15,30,35,43)){
+  sub <- filter(merged.data, date == date.x)
+  sub <- sub[is.finite(sub$zp),]
+  sem <- psem(
+    lm(zoo ~ sc.gly*sc.imi, data = sub),
+    lm(phyto ~ sc.gly + nut.num + zoo, data = sub),
+    data = sub
+  ) 
+  cat(paste('#\n#\n================================ DAY',date.x,'================================\n#\n#'))
+  print(summary(sem,.progressBar = F))
+}
